442b0fc44d66efbd1063a4c2620c76f8fa00665f
Baghi <infcbaghi81@gmail.com>
2023-02-15 22:25:27 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    const [color, setColor] = useState("green");
+
+    const handleDecrement = (event) => {
+        if (count === 0) {
+            return;
+        }
+
+        setCount(count - 1);
+    };
+
+    const handleIncrement = () => {
+        if (count === 10) return;
+        setCount(count + 1);
+    };
+
+    useEffect(() => {
+        if (count >= 0 && count <= 4) setColor("green");
+        else if (count >= 5 && count <= 9) setColor("blue");
+        else setColor("red");
+    }, [count]);
+
+    return ( <
+        div className = "App" >
+        <
+        div className = "container" >
+        <
+        button onClick = { handleDecrement } > - < /button> <
+        h1 className = { `color-${color}` } > { count } < /h1> <
+        button onClick = { handleIncrement } > + < /button> < /
+        div > <
+        /div>
+    );
 }
 
 export default App;
